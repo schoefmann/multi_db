44684b892000b42ec9309b188252c8e83b58e0d1
David Palm <dpalm@elctech.com>
2010-02-10 18:25:59 +0800

    Cleanup

--- lib/multi_db/weighted_scheduler.rb
+++ lib/multi_db/weighted_scheduler.rb
@@ -8,9 +8,7 @@ module MultiDb
   protected
     
     def next_index!
-      # self.current_index = super if total_weight == 2
       rnd_idx = rand(total_weight)
-      # puts "Using rnd_idx: #{rnd_idx}, current_index: #{current_index}<br>"
       self.current_index = items.index(items.detect do |slave|
         rnd_idx -= slave::WEIGHT
         true if rnd_idx < 0
