3ceb7bd768cdec589ef6cf94bf4c16b6fd201e49
Andre Duffeck <aduffeck@suse.de>
2011-08-12 14:59:42 +0200

    fix Rails 3.1 compatibility by getting rid of the RAILS_* constants.
    The specs still pass.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,11 @@ require 'rubygems'
 gem 'activerecord', '3.0.5'
 %w[tlattr_accessors active_record yaml erb rspec logger].each {|lib| require lib}
 
-RAILS_ENV = ENV['RAILS_ENV'] = 'test'
+module Rails
+  def self.env
+    ActiveSupport::StringInquirer.new("test")
+  end
+end
 
 MULTI_DB_SPEC_DIR = File.dirname(__FILE__)
 MULTI_DB_SPEC_CONFIG = YAML::load(File.open(MULTI_DB_SPEC_DIR + '/config/database.yml'))
