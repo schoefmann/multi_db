a797c3422a7a707fad4cc0e03c0a4fa71446a36e
Beyond <beyond@be.to>
2011-08-16 00:50:59 +0900

    enabled query cache

--- lib/multi_db/query_cache_compat.rb
+++ lib/multi_db/query_cache_compat.rb
@@ -3,18 +3,23 @@ module MultiDb
   module QueryCacheCompat
     def select_all(*a, &b)
       next_reader! unless ConnectionProxy.sticky_slave
-      send_to_current(:select_all, *a, &b)
+      cache_sql(a.first) {
+        send_to_current(:select_all, *a, &b)
+      }
     end
     def columns(*a, &b)
       send_to_current(:columns, *a, &b)
     end
     def insert(*a, &b)
+      clear_query_cache
       send_to_master(:insert, *a, &b)
     end
     def update(*a, &b)
+      clear_query_cache
       send_to_master(:update, *a, &b)
     end
     def delete(*a, &b)
+      clear_query_cache
       send_to_master(:delete, *a, &b)
     end
   end
